54c816594843e8875e7bd99cd3c87faa7da87297
Kabece <kabece@gmail.com>
2016-06-01 10:38:03 +0200

    File browser added.

--- Datasety/src/application/Main.java
+++ Datasety/src/application/Main.java
@@ -1,26 +1,102 @@
 package application;
-	
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+
+	private Desktop desktop = Desktop.getDesktop();
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			primaryStage.setTitle("Apka");
+
+			// FileBrowser Part Start
+			final FileChooser fileChooser = new FileChooser();
+
+	        final Button openButton = new Button("Open a File");
+	        final Button openMultipleButton = new Button("Open multiple Files");
+
+	        openButton.setOnAction(
+	            new EventHandler<ActionEvent>() {
+	                @Override
+	                public void handle(final ActionEvent e) {
+	                    File file = fileChooser.showOpenDialog(primaryStage);
+	                    if (file != null) {
+	                        openFile(file);
+	                    }
+	                }
+	            });
+
+	        openMultipleButton.setOnAction(
+	            new EventHandler<ActionEvent>() {
+	                @Override
+	                public void handle(final ActionEvent e) {
+	                    List<File> list =
+	                        fileChooser.showOpenMultipleDialog(primaryStage);
+	                    if (list != null) {
+	                        for (File file : list) {
+	                            openFile(file);
+	                        }
+	                    }
+	                }
+	            });
+
+
+	        final GridPane inputGridPane = new GridPane();
+
+	        GridPane.setConstraints(openButton, 0, 0);
+	        GridPane.setConstraints(openMultipleButton, 1, 0);
+	        inputGridPane.setHgap(6);
+	        inputGridPane.setVgap(6);
+	        inputGridPane.getChildren().addAll(openButton, openMultipleButton);
+
+	        final Pane rootGroup = new VBox(12);
+	        rootGroup.getChildren().addAll(inputGridPane);
+	        rootGroup.setPadding(new Insets(12, 12, 12, 12));
+
+			// FileBrowser Part Finish
+
+	        Scene scene = new Scene(rootGroup,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	private void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(
+                Main.class.getName()).log(
+                    Level.SEVERE, null, ex
+                );
+        }
+    }
 }
