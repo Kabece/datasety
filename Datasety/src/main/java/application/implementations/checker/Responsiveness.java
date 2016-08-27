4bd3bd348d83fe6e387ae43981e7d5b2d2b3560b
Nevaan <pawel.losek@icloud.com>
2016-08-27 23:40:30 +0200

    Implemented Responsiveness pattern

--- Datasety/src/main/java/application/implementations/checker/Responsiveness.java
+++ Datasety/src/main/java/application/implementations/checker/Responsiveness.java
@@ -3,20 +3,79 @@ package application.implementations.checker;
 import application.interfaces.checker.Checker;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by pawel on 26.08.2016.
  * <p>
  *     Implementacja wzorca responsywność
+ *
  * </p>
  *
  */
+
+/*
+        TODO: chyba zeby to robilo wiekszy sens, powinno sie sortować dataset po timestampach.
+        Na ten moment zakładam, że dataset jest posortowany
+*/
+
 public class Responsiveness extends Checker{
 
     public boolean checkPattern(Map<String, ArrayList<String>> dataMap) {
 
-        return false;
+        logger.info("Starting checking responsiveness pattern ...");
+        List<String> values = dataMap.get(logicSentence.getChosenVariable());
 
+        switch (logicSentence.getChosenOperator()) {
+            case EQ:
+
+                for (String value : values) {
+                    if (value.equals(logicSentence.getChosenValue())) {
+                        return checkSuccessorEvent(dataMap.get(logicSentence.getNextSentencePart().getChosenVariable()), values.indexOf(value));
+                    }
+                }
+                break;
+            case NE:
+                for (String value : values) {
+                    if (!value.equals(logicSentence.getChosenValue())) {
+                        return checkSuccessorEvent(dataMap.get(logicSentence.getNextSentencePart().getChosenVariable()), values.indexOf(value));
+                    }
+                }
+                break;
+            default:
+                logger.warn(
+                        "Warning in checkResponsiveness, default switch option used! chosenOperator={}, chosenVariable={}, chosenValue={}",
+                        logicSentence.getChosenOperator(), logicSentence.getChosenVariable(), logicSentence.getChosenValue());
+        }
+
+        logger.info("Finished checking responsiveness pattern!");
+        return false;
+    }
+
+    private boolean checkSuccessorEvent(List<String> values, int recordPosition) {
+
+        switch (logicSentence.getNextSentencePart().getChosenOperator()) {
+            case EQ:
+                for (int i = recordPosition; i < values.size(); i++) {
+                    if (values.get(i).equals(logicSentence.getNextSentencePart().getChosenValue())) {
+                       return true;
+                    }
+                }
+                break;
+            case NE:
+                for (int i = recordPosition; i < values.size(); i++) {
+                    if (!values.get(i).equals(logicSentence.getNextSentencePart().getChosenValue())) {
+                        return true;
+                    }
+                }
+                break;
+            default:
+                logger.warn(
+                        "Warning while checking successor event (Responsiveness pattern), default switch option used! chosenOperator={}, chosenVariable={}, chosenValue={}",
+                        logicSentence.getNextSentencePart().getChosenOperator(), logicSentence.getNextSentencePart().getChosenVariable(), logicSentence.getNextSentencePart().getChosenValue());
+        }
+        return false;
     }
 }
